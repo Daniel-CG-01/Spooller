abcb26288a3bdd9d866885c9de7ea950f74ce007
DanielZR05 <danielzanon2005@gmail.com>
2025-09-25 11:47:13 +0200

    Refactor HikariConfiguration to initialize DataSource lazily and remove unused class files

--- src/main/java/net/ausiasmarch/connection/HikariConfiguration.java
+++ src/main/java/net/ausiasmarch/connection/HikariConfiguration.java
@@ -12,26 +12,25 @@ public class HikariConfiguration {
 
     private static final HikariDataSource webappPool;
 
-    static {
-        HikariConfig config = new HikariConfig();
-
-        // Parámetros básicos de conexión
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/spooller");
-        config.setUsername("root");
-        config.setPassword("tiger");
-
-        // Configuración básica de pool
-        config.setMaximumPoolSize(10);          // Máximo de conexiones en el pool
-        config.setMinimumIdle(2);               // Conexiones mínimas en espera
-        config.setIdleTimeout(30000);           // 30s tiempo máximo que una conexión puede estar inactiva
-        config.setConnectionTimeout(30000);     // 30s para esperar a obtener una conexión
-        config.setPoolName("MiHikariPool");
-
-        webappPool = new HikariDataSource(config);
-    }
-
     // Método para obtener el DataSource (acceso global)
     public static DataSource getPool() {
+        if (webappPool == null) {
+            HikariConfig config = new HikariConfig();
+
+            // Parámetros básicos de conexión
+            config.setJdbcUrl("jdbc:mysql://localhost:3306/spooller");
+            config.setUsername("root");
+            config.setPassword("tiger");
+
+            // Configuración básica de pool
+            config.setMaximumPoolSize(10);          // Máximo de conexiones en el pool
+            config.setMinimumIdle(2);               // Conexiones mínimas en espera
+            config.setIdleTimeout(30000);           // 30s tiempo máximo que una conexión puede estar inactiva
+            config.setConnectionTimeout(30000);     // 30s para esperar a obtener una conexión
+            config.setPoolName("MiHikariPool");
+
+            webappPool = new HikariDataSource(config);
+        }
         return webappPool;
     }
 
